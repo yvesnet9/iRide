51eee9de2665688985b79f67c0ab753e699d7f90
yvesnet9 <yvesnet9@gmail.com>
2025-12-15 03:34:40 +0100

    Add env debug endpoint

---
+++ public/api/debug_env.php
@@ -0,0 +1,11 @@
+<?php
+header('Content-Type: application/json');
+
+echo json_encode([
+    'DATABASE_URL' => getenv('DATABASE_URL'),
+    'DB_CONNECTION' => getenv('DB_CONNECTION'),
+    'DB_HOST' => getenv('DB_HOST'),
+    'DB_DATABASE' => getenv('DB_DATABASE'),
+    'DB_USERNAME' => getenv('DB_USERNAME'),
+    'DB_PASSWORD_SET' => getenv('DB_PASSWORD') ? 'yes' : 'no',
+]);
